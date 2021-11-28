9fa8ea554288b4e997cb72cb42ef84c658a58a63
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-11-28 20:56:18 +0000

    Animations for every weapon, Shoot on the staff still have to be done, UI, AI, Level Design still have to be done

--- GEPProjectSem1/Assets/Scripts/Char_Phys.cs
+++ GEPProjectSem1/Assets/Scripts/Char_Phys.cs
@@ -15,7 +15,7 @@ public class Char_Phys : MonoBehaviour
     private float m_StopingForce = 10f;
     private float m_MaxSpeed = 8f;
     private float m_RotationSpeed;
-    private float m_JumpForce = 10f;
+    private float m_JumpForce = 1f;
     private bool m_IsGrounded;
     public bool m_IsAttacking = false;
     
@@ -78,6 +78,14 @@ public class Char_Phys : MonoBehaviour
                 {
                     m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.HAMMER_RUN);
                 }
+                else if(m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.SPEAR)
+                {
+                    m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.SPEAR_RUN);
+                }
+                else if (m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.STAFF)
+                {
+                    m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.STAFF_RUN);
+                }
                 else
                 {
                     m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.RUN);
@@ -105,12 +113,22 @@ public class Char_Phys : MonoBehaviour
 
         m_Animator.SetFloat("RunSpeed", m_RB.velocity.magnitude / m_MaxSpeed);
         m_Animator.SetFloat("HammerRunSpeed", m_RB.velocity.magnitude / m_MaxSpeed);
+        m_Animator.SetFloat("SpearRunSpeed", m_RB.velocity.magnitude / m_MaxSpeed);
+        m_Animator.SetFloat("StaffRunSpeed", m_RB.velocity.magnitude / m_MaxSpeed);
 
         if (m_RB.velocity == Vector3.zero && !m_IsAttacking)
         {
             if(m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.HAMMER)
             {
                 m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.HAMMER_IDLE);
+            }
+            else if(m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.SPEAR)
+            {
+                m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.SPEAR_IDLE);
+            }
+            else if (m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.STAFF)
+            {
+                m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.STAFF_IDLE);
             }
             else
             {
@@ -123,9 +141,8 @@ public class Char_Phys : MonoBehaviour
         
         if(IsGrounded() && Input.GetButton("Jump"))
         {
-            m_RB.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
             m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.JUMP);
-
+            m_RB.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
         }
 
 
@@ -151,13 +168,13 @@ public class Char_Phys : MonoBehaviour
         {
             if (Input.GetButton("Fire1"))
             {
-                
+
                 m_Animator.SetTrigger("Normal_Attack_Axe_Mace");
-                
+
 
             }
         }
-        else if(m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.HAMMER)
+        else if (m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.HAMMER)
         {
             if (Input.GetButton("Fire1"))
             {
@@ -167,16 +184,27 @@ public class Char_Phys : MonoBehaviour
 
             }
         }
+        else if (m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.SPEAR)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+
+                m_Animator.SetTrigger("Normal_Attack_Spear");
+
+
+            }
+        }
+        else if (m_Character_Weapon_Controller.m_EquipedWeapon == PickUpWeaponType.STAFF)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+
+                m_Animator.SetTrigger("Normal_Attack_Staff");
+
+
+            }
+        }
     }
-
-    //private IEnumerator Attack()
-    //{
-    //    m_PlayerAnimationController.ChangeAnimationState(m_PlayerState = PlayerState.NORMALATTACK);
-    //    yield return new WaitForSeconds(1.4f);
-    //    m_IsAttacking = false;
-
-    //}
-
 }
  
 
@@ -184,10 +212,16 @@ public enum PlayerState
 {
     IDLE,
     HAMMER_IDLE,
+    SPEAR_IDLE,
+    STAFF_IDLE,
     RUN,
     HAMMER_RUN,
+    SPEAR_RUN,
+    STAFF_RUN,
     JUMP,
     NORMAL_ATTACK_AXE_MACE,
     NORMAL_ATTACK_HAMMER,
+    NORMAL_ATTACK_SPEAR,
+    NORMAL_ATTACK_STAFF,
     DEATH
 }
